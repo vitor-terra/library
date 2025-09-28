2b22daa0681e89fcef9d9de4d508bb1c468a7084
e <vtmpv2210@gmail.com>
2025-09-28 04:37:26 -0300

    Commit 5: Extracting get_frequent_renter_points from Client.statement to Rental.

--- model.py
+++ model.py
@@ -27,6 +27,12 @@ class Rental:
             if self.days_rented > 3:
                 amount += (self.days_rented - 3) * 1.5
         return amount
+    
+    def get_frequent_renter_points(self):
+        points = 1
+        if self.book.price_code == Book.NEW_RELEASE and self.days_rented > 1:
+            points += 1
+        return points
 
 class Client:
 
@@ -46,10 +52,7 @@ class Client:
         for rental in self.rentals:
             amount = rental.get_charge()
 
-            # add frequent renter points
-            frequent_renter_points += 1
-            if rental.book.price_code == Book.NEW_RELEASE and rental.days_rented > 1:
-                frequent_renter_points += 1
+            frequent_renter_points += rental.get_frequent_renter_points()
 
             # show each rental result
             result += f"- {rental.book.title}: {amount}\n"
